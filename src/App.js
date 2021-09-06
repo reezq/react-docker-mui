18445f6b79562453b128caeb088e2bf961a78e0d
reezq <reezcliquers@gmail.com>
2021-09-06 22:27:00 +0700

    tambah component switch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import Switch from '@mui/material/Switch';
+import * as React from 'react';
+
 
 function App() {
+
+const [success, setSuccess] = React.useState(false)
+const mainPrimaryColor = success ? '#d91a57' : '#008f4b'
+const palletype = success ? 'dark' : 'light'
+const theme = createTheme({
+  palette: {
+    mode: palletype,
+      primary: {
+        main: mainPrimaryColor
+  }},
+});
+
+const handleChange = () => {
+  setSuccess(!success);
+};
+console.log(palletype);
+console.log(mainPrimaryColor);
   return (
+
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h3>{theme.palette.mode} <code>src/App.js</code> and save to reload.</h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Switch
+            checked={success}
+            onChange={handleChange}
+          />
+        <Button variant="contained">Ganti Warna</Button>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
